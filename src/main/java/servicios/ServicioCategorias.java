938bbba13d75c82a75d5be2397c3d7f0c7504b31
Jiahui <jiahui.lin@um.es>
2025-11-11 17:02:29 +0100

    Implementación servicio categorías

--- src/main/java/servicios/ServicioCategorias.java
+++ src/main/java/servicios/ServicioCategorias.java
@@ -5,45 +5,84 @@ import java.util.List;
 import dominio.Categoria;
 import repositorios.EntidadNoEncontrada;
 import repositorios.FactoriaRepositorios;
-import repositorios.RepositorioCategoriasJPA;
+import repositorios.RepositorioCategoriasAdHocJPA;
 import repositorios.RepositorioException;
 import utils.CargarCategorias;
 import utils.ICargarCategorias;
 
 public class ServicioCategorias implements IServicioCategorias{
 
-	RepositorioCategoriasJPA repositorio = FactoriaRepositorios.getRepositorio(Categoria.class);
-	
+	RepositorioCategoriasAdHocJPA repositorio = FactoriaRepositorios.getRepositorio(Categoria.class);
+		
 	@Override
 	public boolean cargarCategorias(String ruta) {
 		ICargarCategorias cargar = new CargarCategorias();
 		Categoria raiz = cargar.cargarCategorias(ruta);
 		if (raiz == null) return false;
+		
 		try {
-			repositorio.add(raiz);
-			return true;
+			try {
+				repositorio.getById(raiz.getId());
+
+				System.err.println("La categoría principal " + raiz.getId() + " ya existe. No se cargará.");
+				return false; 
+			} catch (EntidadNoEncontrada e) {
+				repositorio.add(raiz);
+				return true;
+			}
 		} catch (RepositorioException e) {
-			System.out.println("Error a la hora de añadir la categoría");
+			System.err.println("Error al añadir la categoría: " + e.getMessage());
+			e.printStackTrace();
 			return false;
 		}
 	}
 
 	@Override
 	public boolean modificarCategoria(String idCategoria, String descripcion) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			Categoria categoria = repositorio.getById(idCategoria);
+			categoria.setDescripcion(descripcion);
+	
+			repositorio.update(categoria);
+			
+			return true;
+			
+		} catch (EntidadNoEncontrada e) {
+			System.err.println("Error al modificar: No se encontró la categoría con id " + idCategoria);
+			e.printStackTrace();
+			return false;
+		} catch (RepositorioException e) {
+			System.err.println("Error de repositorio al modificar la categoría: " + e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public List<Categoria> recuperarCategoriasRaiz() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return repositorio.getCategoriasRaiz();
+		} catch (RepositorioException e) {
+			System.err.println("Error al recuperar categorías raíz: " + e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
 	public List<Categoria> recuperarCategoriasDescendentes(String idCategoria) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Categoria categoriaPadre = repositorio.getById(idCategoria);
+			return repositorio.getDescendientes(categoriaPadre);
+			
+		} catch (EntidadNoEncontrada e) {
+			System.err.println("Error al buscar descendientes: No se encontró la categoría con id " + idCategoria);
+			e.printStackTrace();
+			return null;
+		} catch (RepositorioException e) {
+			System.err.println("Error de repositorio al buscar descendientes: " + e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
 	}
-
 }
