44eefbea6ab1936e2821452c6dbfb28143c17150
Jiahui <jiahui.lin@um.es>
2025-11-11 16:39:54 +0100

    Implementación repositorio categorías

--- src/main/java/repositorios/RepositorioCategoriasAdHoc.java
+++ src/main/java/repositorios/RepositorioCategoriasAdHoc.java
@@ -1,7 +1,13 @@
 package repositorios;
 
+import java.util.List;
+
+import dominio.Categoria;
+
 public interface RepositorioCategoriasAdHoc {
 
+	public List<Categoria> getCategoriasRaiz() throws RepositorioException;
 	
+	public List<Categoria> getDescendientes(Categoria categoriaPadre) throws RepositorioException;
 	
 }
