5d04865eb8a6ac11ddffd1c088f6c3f27adca5fa
jesus <jesussanchezz0004@gmail.com>
2025-12-03 18:55:52 +0100

    Configuración del proyecto para la entrega 2.

--- src/main/java/servicios/IServicioUsuarios.java
+++ src/main/java/servicios/IServicioUsuarios.java
@@ -11,4 +11,6 @@ public interface IServicioUsuarios {
 	public boolean modificarUsuario(String idUsuario, String nombre, String apellido,String correo,String clave, LocalDate fecha, String telefono);
 	
 	public Usuario recuperarUsuario(String idUsuario);
+	
+
 }
